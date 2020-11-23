b247f5d6dd5de24e10ffe1b8036ba6af270e0fc4
Sajeeb <sajeebahamed6@gmail.com>
2020-11-24 02:22:48 +0600

    core useReducer done

--- redux-practice-1/src/components/PatientCom/PatientCom.js
+++ redux-practice-1/src/components/PatientCom/PatientCom.js
@@ -1,12 +1,38 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useRef } from 'react';
 import { patientReducer, patientState } from '../reducers/patientReducer';
 
 const PatientCom = () => {
+    const nameRef = useRef()
     const [state, dispatch] = useReducer(patientReducer, patientState)
     console.log(state);
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch({
+            type: 'ADD_PATIENT',
+            id: state.patients.length + 1,
+            name: nameRef.current.value
+        })
+        console.log(nameRef.current.value)
+        nameRef.current.value = ''
+    }
     return (
         <div>
-            <h1> {state.patients.length} </h1>
+           {
+               state.patients.map(patient=> <li
+                key={patient.id}
+                onClick={ () => dispatch({ type: 'REMOVE_PATIENT', id: patient.id }) }
+               >
+                {patient.name}
+                
+            </li>)
+           }
+            <form onSubmit={handleSubmit}>
+                <input
+                    ref={nameRef}
+                    type="text"
+                
+                />
+            </form>
         </div>
     );
 };
--- redux-practice-1/src/components/reducers/patientReducer.js
+++ redux-practice-1/src/components/reducers/patientReducer.js
@@ -4,9 +4,16 @@ export const patientState = {
 export const patientReducer = (state, action) => {
     switch (action.type) {
         case 'ADD_PATIENT':
-            return state
+            const newPatient = {
+                id: action.id,
+                name: action.name
+            }
+            const allPatient = [...state.patients, newPatient]
+            return { patients: allPatient }
         case 'REMOVE_PATIENT': 
-            return state
+            const remainignPatient = state.patients.filter(patient=> patient.id !== action.id)
+            const newState = { patients: remainignPatient }
+            return newState
         default:
             return state
     }
